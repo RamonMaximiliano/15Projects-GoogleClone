af3e178c7499ed7012b47b913d1802204d7b7c70
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-03-01 22:37:23 -0300

    menu

---
+++ script.js
@@ -0,0 +1,52 @@
+/* menu-home*/
+function showHome() {
+    document.querySelector("p.menu-home").classList.remove("class-hidden")
+    document.querySelector("p.menu-home").classList.add("class-visible")
+}
+
+function hideHome() {
+    document.querySelector("p.menu-home").classList.remove("class-visible")
+    document.querySelector("p.menu-home").classList.add("class-hidden")
+}
+/* menu-home*/
+
+/* menu-google*/
+function showGoogle() {
+    document.querySelector("p.menu-Google").classList.remove("class-hidden")
+    document.querySelector("p.menu-Google").classList.add("class-visible")
+}
+
+function hideGoogle() {
+    document.querySelector("p.menu-Google").classList.remove("class-visible")
+    document.querySelector("p.menu-Google").classList.add("class-hidden")
+}
+/* menu-google*/
+
+
+/* menu-fox*/
+function showFox() {
+    document.querySelector("p.menu-Fox").classList.remove("class-hidden")
+    document.querySelector("p.menu-Fox").classList.add("class-visible")
+}
+
+function hideFox() {
+    document.querySelector("p.menu-Fox").classList.remove("class-visible")
+    document.querySelector("p.menu-Fox").classList.add("class-hidden")
+}
+/* menu-fox*/
+
+/* menu-about*/
+function showAbout() {
+    document.querySelector("p.menu-About").classList.remove("class-hidden")
+    document.querySelector("p.menu-About").classList.add("class-visible")
+}
+
+function hideAbout() {
+    document.querySelector("p.menu-About").classList.remove("class-visible")
+    document.querySelector("p.menu-About").classList.add("class-hidden")
+}
+/* menu-about*/
+
+
+
+
